c26f88b708da231eded196532cdb5dcf827a9e6e
kgobin2000 <44628334+kgobin2000@users.noreply.github.com>
2022-03-21 21:41:48 -0400

    adding custom submit button

--- screens/reviewForm.js
+++ screens/reviewForm.js
@@ -3,8 +3,9 @@ import { StyleSheet, Button, View, TextInput, Text } from "react-native";
 import { globalStyles } from "../styles/global.js";
 import { Formik } from "formik";
 import * as yup from "yup";
+import FlatButton from "../shared/button";
 
-const ReviewSchema = yup.object({
+const reviewSchema = yup.object({
   title: yup
     .string()
     .required()
@@ -12,13 +13,20 @@ const ReviewSchema = yup.object({
   body: yup
     .string()
     .required()
-    .min(8)
+    .min(8),
+  rating: yup
+    .string()
+    .required()
+    .test("is-num-1-5", "Rating must be a number 1 - 5", val => {
+      return parseInt(val) < 6 && parseInt(val) > 0;
+    })
 });
 
 export default function ReviewForm({ addReview }) {
   return (
     <View style={globalStyles.container}>
       <Formik
+        validationSchema={reviewSchema}
         initialValues={{ title: "", body: "", rating: "" }}
         onSubmit={values => {
           addReview(values);
@@ -27,22 +35,34 @@ export default function ReviewForm({ addReview }) {
       >
         {props => (
           <View>
-            <TextInput style={globalStyles.input} placeholder="Review Title" onChangeText={props.handleChange("title")} value={props.values.title} />
+            <TextInput
+              style={globalStyles.input}
+              placeholder="Review Title"
+              onChangeText={props.handleChange("title")}
+              value={props.values.title}
+              onBlur={props.handleBlur("title")}
+            />
+            <Text style={globalStyles.errorText}>{props.touched.title && props.errors.title}</Text>
             <TextInput
               multiline
               style={globalStyles.input}
               placeholder="Review Body"
               onChangeText={props.handleChange("body")}
               value={props.values.body}
+              onBlur={props.handleBlur("body")}
             />
+            <Text style={globalStyles.errorText}>{props.touched.body && props.errors.body}</Text>
+
             <TextInput
               style={globalStyles.input}
               placeholder="Rating (1-5)"
               onChangeText={props.handleChange("rating")}
               value={props.values.rating}
               keyboardType="numeric"
+              onBlur={props.handleBlur("rating")}
             />
-            <Button title="Submit" color="maroon" onPress={props.handleSubmit} />
+            <Text style={globalStyles.errorText}>{props.touched.rating && props.errors.rating}</Text>
+            <FlatButton text="submit" onPress={props.handleSubmit} />
           </View>
         )}
       </Formik>
